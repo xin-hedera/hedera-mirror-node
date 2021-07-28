9399ab3792789e59ab8ceb375af7b8b626d47ef6
Xin Li <xin.li@hedera.com>
2021-07-28 14:34:09 -0500

    distributed hypertables
    Signed-off-by: Xin Li <xin.li@hedera.com>

---
+++ hedera-mirror-importer/src/main/resources/db/migration/v2/V2.0.1__distributed_hypertables.sql
@@ -0,0 +1,125 @@
+-------------------
+-- Create hyper tables for tables that have mostly insert logic
+-- Set chunk_time_interval using parameterized value, usually default of 604800000000000 ns (7 days)
+-- Set create_default_indexes to false for tables where a primary key is needed or an index in ASC order is needed.
+-- By default TimescaleDB adds an index in DESC order for partitioning column
+-------------------
+
+-- assessed_custom_fee
+select create_distributed_hypertable('assessed_custom_fee', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- account_balance
+select create_distributed_hypertable('account_balance', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- account_balance_file
+select create_distributed_hypertable('account_balance_file', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- address_book hyper table creation skipped as small tables won't benefit from timescaledb scalability
+
+-- address_book_entry hyper table creation skipped as small tables won't benefit from timescaledb scalability
+
+-- address_book_service_endpoint hyper table creation skipped as small tables won't benefit from timescaledb scalability
+
+-- contract_result
+select create_distributed_hypertable('contract_result', 'consensus_timestamp', 'consensus_timestamp',
+                          chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- crypto_transfer
+select create_distributed_hypertable('crypto_transfer', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- custom_fee
+select create_distributed_hypertable('custom_fee', 'created_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- event_file
+select create_distributed_hypertable('event_file', 'consensus_end', 'consensus_end',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- file_data
+select create_distributed_hypertable('file_data', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- live_hash
+select create_distributed_hypertable('live_hash', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- nft
+select create_hypertable('nft', 'created_timestamp', chunk_time_interval => ${chunkTimeInterval},
+                         create_default_indexes => false, if_not_exists => true);
+
+-- nft_transfer
+select create_distributed_hypertable('nft_transfer', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- non_fee_transfer
+select create_distributed_hypertable('non_fee_transfer', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- record_file
+select create_distributed_hypertable('record_file', 'consensus_end', 'consensus_end',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- schedule
+select create_distributed_hypertable('schedule', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- entity
+select create_hypertable('entity', 'id', chunk_time_interval => ${chunkIdInterval},
+                         create_default_indexes => false, if_not_exists => true);
+
+-- t_entity_types hyper table creation skipped as it serves only as a reference table and rarely gets updated
+
+-- t_transaction_results hyper table creation skipped as it serves only as a reference table and rarely gets updated
+
+-- t_transaction_types hyper table creation skipped as it serves only as a reference table and rarely gets updated
+
+-- token
+select create_hypertable('token', 'created_timestamp', chunk_time_interval => ${chunkTimeInterval},
+                         create_default_indexes => false, if_not_exists => true);
+
+-- token_account
+select create_distributed_hypertable('token_account', 'created_timestamp', 'created_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- token_balance
+select create_distributed_hypertable('token_balance', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- token_transfer
+select create_distributed_hypertable('token_transfer', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- topic_message
+select create_distributed_hypertable('topic_message', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
+
+-- transaction
+select create_distributed_hypertable('transaction', 'consensus_ns', 'consensus_ns',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                          if_not_exists => true);
+
+-- transaction_signature
+select create_distributed_hypertable('transaction_signature', 'consensus_timestamp', 'consensus_timestamp',
+                         chunk_time_interval => ${chunkTimeInterval}, create_default_indexes => false,
+                         if_not_exists => true);
