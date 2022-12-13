81036fc461ee32475a84a8e29fd67a337973c438
Xin Li <xin.li@swirldslabs.com>
2022-12-13 15:04:38 -0600

    change report.md path
    Signed-off-by: Xin Li <xin.li@swirldslabs.com>

--- hedera-mirror-test/k6/src/rest/apis.js
+++ hedera-mirror-test/k6/src/rest/apis.js
@@ -28,7 +28,7 @@ import {setupTestParameters} from './test/bootstrapEnvParameters.js';
 function handleSummary(data) {
   return {
     stdout: textSummary(data, {indent: ' ', enableColors: true}),
-    '/data/reports/report.md': markdownReport(data, true, scenarios),
+    '/share/reports/report.md': markdownReport(data, true, scenarios),
   };
 }
 
