bd075e87933dca2702bf8f38dff7a2bddcd2dd57
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2025-03-07 11:04:43 -0600

    Upload coverage reports to Codacy (#10563)
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- buildSrc/src/main/kotlin/common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/common-conventions.gradle.kts
@@ -19,3 +19,26 @@ dependencyCheck {
     }
     nvd { datafeedUrl = "https://dependency-check.github.io/DependencyCheck_Builder/nvd_cache/" }
 }
+
+tasks.register<Exec>("uploadCoverage") {
+    description = "Uploads coverage report"
+    group = "verification"
+    var args = ""
+
+    if (pluginManager.hasPlugin("java-conventions")) {
+        args = "-l Java -r build/reports/jacoco/test/jacocoTestReport.xml"
+    } else if (pluginManager.hasPlugin("javascript-conventions")) {
+        args = "-l Javascript -r build/coverage/lcov.info"
+    } else if (pluginManager.hasPlugin("go-conventions")) {
+        args = "-l Go --force-coverage-parser go -r coverage.txt"
+    }
+
+    commandLine(
+        listOf(
+            "bash",
+            "-c",
+            "bash <(curl -Ls https://coverage.codacy.com/get.sh) report --partial $args",
+        )
+    )
+    onlyIf { System.getenv().containsKey("CI") && System.getenv("CODACY_PROJECT_TOKEN") != null }
+}
