78450dd974846e1020b9d92537cfd17b7b41443d
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2025-08-13 11:07:30 -0500

    Cleanup web3 temp directory on startup (#11771)
    * Change default temporary directory to application specific `/tmp/web3`
    * Cleanup web3 temp directory on startup
    * Lower tmp volume memory size from 500 MiB to 100 MiB
    
    ---------
    
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- web3/src/main/java/org/hiero/mirror/web3/Web3Application.java
+++ web3/src/main/java/org/hiero/mirror/web3/Web3Application.java
@@ -2,16 +2,32 @@
 
 package org.hiero.mirror.web3;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import org.hiero.mirror.common.CommonConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.util.FileSystemUtils;
 
 @Import(CommonConfiguration.class)
 @SpringBootApplication
 public class Web3Application {
 
     public static void main(String[] args) {
+        cleanup();
         SpringApplication.run(Web3Application.class, args);
+    }
+
+    private static void cleanup() {
+        final var tmpDir = Path.of(System.getProperty("java.io.tmpdir", "/tmp/web3"));
+
+        try {
+            FileSystemUtils.deleteRecursively(tmpDir);
+            Files.createDirectories(tmpDir);
+        } catch (IOException ex) {
+            System.err.println("Could not delete tmp directory %s: %s".formatted(tmpDir, ex.getMessage()));
+        }
     }
 }
