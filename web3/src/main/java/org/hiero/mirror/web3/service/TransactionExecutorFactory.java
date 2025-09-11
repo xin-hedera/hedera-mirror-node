6a18c747c59dabe7f380bee9f36991045befcb70
Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>
2025-09-11 17:24:01 +0300

    Simplify the web3 app warmup (#11938)
    Due to the high number of logged errors on application warmup, this PR removes the original implementation and replaces it with a more simpler one.
    
    ---------
    
    Signed-off-by: Bilyana Gospodinova <bilyana.gospodinova14@gmail.com>

--- web3/src/main/java/org/hiero/mirror/web3/service/TransactionExecutorFactory.java
+++ web3/src/main/java/org/hiero/mirror/web3/service/TransactionExecutorFactory.java
@@ -13,9 +13,13 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import lombok.RequiredArgsConstructor;
+import org.hiero.mirror.web3.common.ContractCallContext;
 import org.hiero.mirror.web3.evm.config.ModularizedOperation;
 import org.hiero.mirror.web3.evm.properties.MirrorNodeEvmProperties;
 import org.hiero.mirror.web3.state.MirrorNodeState;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 
 @Named
 @RequiredArgsConstructor
@@ -26,6 +30,19 @@ public class TransactionExecutorFactory {
     private final MirrorNodeEvmProperties mirrorNodeEvmProperties;
     private final Map<SemanticVersion, TransactionExecutor> transactionExecutors = new ConcurrentHashMap<>();
     private final EntityIdFactory entityIdFactory;
+
+    @Async
+    @EventListener(ApplicationReadyEvent.class)
+    public void init() {
+        if (mirrorNodeEvmProperties.isModularizedServices()) {
+            // Create transaction executor for each EVM version, on startup, before the k8s
+            // readiness probe elapses, so we avoid slowing down the initial contract calls.
+            ContractCallContext.run(ctx -> {
+                mirrorNodeEvmProperties.getEvmVersions().values().forEach(this::create);
+                return ctx;
+            });
+        }
+    }
 
     // Reuse TransactionExecutor across requests for the same EVM version
     public TransactionExecutor get() {
