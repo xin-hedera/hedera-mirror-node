e792b753a34a44c8e8698837bf29aafe98615253
Steven Sheehy <17552371+steven-sheehy@users.noreply.github.com>
2025-10-23 12:29:06 -0500

    Bump Spring Boot from 3.5.6 to 3.5.7 (#12258)
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
     implementation("org.jooq:jooq-postgres-extensions:${jooqVersion}")
     implementation("org.openapitools:openapi-generator-gradle-plugin:7.16.0")
     implementation("org.owasp:dependency-check-gradle:12.1.8")
-    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.5.6")
+    implementation("org.springframework.boot:spring-boot-gradle-plugin:3.5.7")
     implementation("org.testcontainers:postgresql:1.21.3")
     implementation("org.web3j:web3j-gradle-plugin:4.14.0")
 }
