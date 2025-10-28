e67cf819f1af269d88bc7c36644500a9699ca13f
Xin Li <59580070+xin-hedera@users.noreply.github.com>
2025-10-28 12:17:19 -0500

    HIP-1056 Set TransactionRecord.evm_address properly (#12288)
    - Don't set evm_address if the alias in the tx body is 20-bytes
    - Derive and set evm_address if derivable from the alias in the tx body
    
    Signed-off-by: Xin Li <xin@hashgraph.com>

--- importer/src/main/java/org/hiero/mirror/importer/downloader/block/transformer/CryptoCreateTransformer.java
+++ importer/src/main/java/org/hiero/mirror/importer/downloader/block/transformer/CryptoCreateTransformer.java
@@ -5,8 +5,10 @@ package org.hiero.mirror.importer.downloader.block.transformer;
 import com.hedera.hapi.block.stream.output.protoc.TransactionOutput;
 import com.hedera.hapi.block.stream.output.protoc.TransactionOutput.TransactionCase;
 import jakarta.inject.Named;
+import org.apache.commons.lang3.ArrayUtils;
 import org.hiero.mirror.common.domain.transaction.TransactionType;
 import org.hiero.mirror.common.util.DomainUtils;
+import org.hiero.mirror.importer.util.Utility;
 
 @Named
 final class CryptoCreateTransformer extends AbstractBlockTransactionTransformer {
@@ -23,16 +25,21 @@ final class CryptoCreateTransformer extends AbstractBlockTransactionTransformer 
                 .getTransactionBody()
                 .getCryptoCreateAccount()
                 .getAlias();
-        if (alias.size() == DomainUtils.EVM_ADDRESS_LENGTH) {
-            recordBuilder.setEvmAddress(alias);
+        if (!alias.isEmpty() && alias.size() != DomainUtils.EVM_ADDRESS_LENGTH) {
+            // This must be a synthetic transaction. The statechanges for its alias and evm address are in the top level
+            // triggering crypto transfer transaction. However, there's no parent link to follow for statechanges
+            // lookup. Instead, the evm address can be calculated from the alias.
+            byte[] evmAddress = Utility.aliasToEvmAddress(DomainUtils.toBytes(alias));
+            if (ArrayUtils.isNotEmpty(evmAddress)) {
+                recordBuilder.setEvmAddress(DomainUtils.fromBytes(evmAddress));
+            }
         }
 
         var accountCreate = blockTransaction
                 .getTransactionOutput(TransactionCase.ACCOUNT_CREATE)
                 .map(TransactionOutput::getAccountCreate)
                 .orElseThrow();
-        var receiptBuilder = recordBuilder.getReceiptBuilder();
-        receiptBuilder.setAccountID(accountCreate.getCreatedAccountId());
+        recordBuilder.getReceiptBuilder().setAccountID(accountCreate.getCreatedAccountId());
     }
 
     @Override
