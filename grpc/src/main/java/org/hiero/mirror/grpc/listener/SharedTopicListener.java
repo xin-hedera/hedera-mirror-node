7443795fbb8fd5f58e626c59099e1b60f7da2e66
Saai Syvendra <157691467+saai-syvendra@users.noreply.github.com>
2026-01-20 23:12:10 +0530

    Replace use of deprecated DirectProcessor (#9630)
    Updated SharedTopicListener to use a Sink Implementation from the deprecated DirectProcessor
    
    ---------
    
    Signed-off-by: Saai Syvendra (Github key) <saaisyvendras@gmail.com>
    Signed-off-by: Saai Syvendra <157691467+saai-syvendra@users.noreply.github.com>
    Signed-off-by: Steven Sheehy <steven.sheehy@swirldslabs.com>
    Co-authored-by: Steven Sheehy <steven.sheehy@swirldslabs.com>

--- grpc/src/main/java/org/hiero/mirror/grpc/listener/SharedTopicListener.java
+++ grpc/src/main/java/org/hiero/mirror/grpc/listener/SharedTopicListener.java
@@ -7,10 +7,8 @@ import org.hiero.mirror.common.domain.topic.TopicMessage;
 import org.hiero.mirror.grpc.domain.TopicMessageFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.Exceptions;
-import reactor.core.publisher.DirectProcessor;
+import reactor.core.publisher.BufferOverflowStrategy;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.FluxSink;
 import reactor.core.scheduler.Schedulers;
 
 @RequiredArgsConstructor
@@ -20,18 +18,10 @@ public abstract class SharedTopicListener implements TopicListener {
     protected final ListenerProperties listenerProperties;
 
     @Override
-    @SuppressWarnings("deprecation")
     public Flux<TopicMessage> listen(TopicMessageFilter filter) {
-        DirectProcessor<TopicMessage> overflowProcessor = DirectProcessor.create();
-        FluxSink<TopicMessage> overflowSink = overflowProcessor.sink();
-
-        // moving publishOn from after onBackpressureBuffer to after Flux.merge reduces CPU usage by up to 40%
-        Flux<TopicMessage> topicMessageFlux = getSharedListener(filter)
+        return getSharedListener(filter)
                 .doOnSubscribe(s -> log.info("Subscribing: {}", filter))
-                .onBackpressureBuffer(
-                        listenerProperties.getMaxBufferSize(), t -> overflowSink.error(Exceptions.failWithOverflow()))
-                .doFinally(s -> overflowSink.complete());
-        return Flux.merge(listenerProperties.getPrefetch(), topicMessageFlux, overflowProcessor)
+                .onBackpressureBuffer(listenerProperties.getMaxBufferSize(), BufferOverflowStrategy.ERROR)
                 .publishOn(Schedulers.boundedElastic(), false, listenerProperties.getPrefetch());
     }
 
